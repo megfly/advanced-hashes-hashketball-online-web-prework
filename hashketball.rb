26b26c7f3969b3d095222c56df5b165567df3da9
Meghan Flynn <mflynn333@gmail.com>
2019-11-21 00:02:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -202,7 +202,8 @@ def player_stats(player_name)
       if attributes == :players 
         data.each do |player|
           if player[:player_name] == players_name 
-            new_hash = player
+            new_hash = player.delete_if do |k, v|
+              k == :player_name
           end 
         end 
       end
